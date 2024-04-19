0cb807589007d86bb05ed74457fe4a31cdac8232
farahBassoumi <bassoumiifarah@gmail.com>
2024-04-19 23:38:59 +0100

    Initial commit

---
+++ src/resolvers/query.ts
@@ -0,0 +1,16 @@
+export const Query = {
+    hello: () => "Hello GL3 2023 2024 :D",
+    getUsers: () => [
+        {
+            name: 'aymen',
+            age: 41,
+            profiles : [
+                {
+                    url: "x/aymenSellaaouti",
+                    socialMedia: "X",
+                }
+            ]
+        }
+    ]
+    
+}
